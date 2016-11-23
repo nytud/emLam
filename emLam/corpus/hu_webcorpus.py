e512f3e364919221c7e3f37f3f6ff9fc24052f84
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2016-11-23 15:45:19 +0100

    Added file for the webcorpus.

---
+++ emLam/corpus/hu_webcorpus.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Preprocessing steps for the Szeged corpus."""
+
+from __future__ import absolute_import, division, print_function
+from html.parser import HTMLParser  # Needs future, too
+
+
+class WebcorpusPreprocessing(Preprocessing):
+    def __init__(self):
+        pass
