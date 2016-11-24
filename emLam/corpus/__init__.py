7d2608464ee23ea29aeac74eb354202ba7e0b1b6
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2016-11-24 18:00:26 +0100

    Do not expose GATEPreprocessor.

--- emLam/corpus/__init__.py
+++ emLam/corpus/__init__.py
@@ -3,10 +3,6 @@ import importlib
 import inspect
 import os
 import os.path as op
-import re
-
-
-__corpus_p = re.compile(r'(.+_.+)[.]py')
 
 
 def get_all_corpora():
@@ -17,9 +13,10 @@ def get_all_corpora():
 
     cdir = op.dirname(op.abspath(__file__))
     for cfile in os.listdir(cdir):
-        m = __corpus_p.match(cfile)
         module_name = inspect.getmodulename(op.join(cdir, cfile))
-        if module_name and module_name != 'corpus_base':
+        # TODO This is ugly; use ABC's or metaprogramming, see
+        # http://python-3-patterns-idioms-test.readthedocs.io/en/latest/Metaprogramming.html
+        if module_name and module_name != 'corpus_base' and module_name != 'gate_corpus':
             module = importlib.import_module(__name__ + '.' + module_name)
             for cls in inspect.getmembers(module, partial(is_mod_class, module)):
                 corpora[module_name] = cls[1]
