a1168cb7f9221cb417932aa48b97cf9e413f4803
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-01-16 18:10:16 +0100

    Default value for an optional argument.

--- emLam/corpus/corpus_base.py
+++ emLam/corpus/corpus_base.py
@@ -11,7 +11,7 @@ from emLam.utils import openall
 
 class Corpus(Component):
     """Base class for corpus objects."""
-    def __init__(self, max_lines):
+    def __init__(self, max_lines=None):
         self.max_lines = max_lines
 
     def instream(self, input_file):
