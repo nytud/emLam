befd2d29263c3ac0696baa7ebbd851e9301f7901
Dávid Márk Nemeskey <nemeskeyd@gmail.com>
2017-03-22 08:07:14 +0100

    inspect-based solution for the names, etc.

--- scripts/compile_input.py
+++ scripts/compile_input.py
@@ -6,10 +6,12 @@
 from __future__ import absolute_import, division, print_function
 from future import standard_library
 from argparse import ArgumentParser
+import inspect
 from itertools import chain
 import os
 import os.path as op
-import subprocess
+import random
+import sys
 
 import numpy as np
 
@@ -20,128 +22,136 @@ standard_library.install_aliases()
 
 
 class CorpusCompiler(object):
-    def __init__(self, input_files, output_prefix):
+    def __init__(self, input_files, batch_size, *args):
+        super(CorpusCompiler, self).__init__(*args)
         self.input_files = input_files
-        self.output_prefix = output_prefix
-        self.input_size = self.input_length(input_files)
+        self.batch_size = batch_size
+        self.batches = [[] for _ in range(batch_size)]
+        self.ctype = get_name_stem(self.__class__, 'CorpusCompiler')
 
-    @staticmethod
-    def input_length(input_files):
-        sum_len = 0
-        for input_file in input_files:
-            if input_file.endswith('.gz'):
-                cmd = 'zcat'
-            elif input_file.endswith('.bz2'):
-                cmd = 'bzcat'
-            else:
-                cmd = 'cat'
-            s = subprocess.getoutput('{} "{}" | wc -wl'.format(cmd, input_file))
-            fields = s.strip().rsplit('\n', 1)[-1].strip().split()
-            sum_len += int(fields[0]) + int(fields[1])
-        return sum_len
+    def compile_corpus(self):
+        raise NotImplementedError('compile_corpus must be implemented')
 
-    @staticmethod
-    def read_input(input_files):
-        """Reads the input files one-by-one and yields tokens line-by-line."""
-        for input_file in input_files:
+    def read_input(self):
+        """Reads all the input into memory."""
+        # TODO back to the iterator
+        ret = []
+        for input_file in self.input_files:
             with openall(input_file) as inf:
-                for line in inf:
-                    yield line.strip().split() + ['</s>']
+                ret.extend([line.split() + ['</s>'] for line in inf])
+        return ret
 
-
-class TxtDiskCompiler(CorpusCompiler):
-    def __init__(self, input_files, output_prefix, batch_size):
-        super(TxtDiskCompiler, self).__init__(input_files, output_prefix)
-        self.batch_size = batch_size
-
-    def __call__(self):
-        """Does the work."""
-        num_outs = self.batch_size  # The modulo is dropped
-        out_size = self.input_size // num_outs
-        out_ext = digits_format_str(num_outs)
-
-        with openall(self.output_prefix, 'wt') as header:
-            print('TXT_DISK\t{}\t{}'.format(
-                num_outs, self.input_size), file=header)
-
-        input_iter = self.read_input(self.input_files)
-        tokens = []
-        for outi in range(num_outs):
-            written = 0
-            with openall(self.output_prefix + out_ext.format(outi), 'wt') as outf:
-                while written < out_size:
-                    tokens.extend(next(input_iter))
-                    tokens_to_write = tokens[:out_size - written]
-                    print(('\n'.join(tokens_to_write)), file=outf)
-                    written += len(tokens_to_write)
-                    tokens = tokens[len(tokens_to_write):]
-
-
-class IntMemCompiler(CorpusCompiler):
-    def __init__(self, input_files, output_prefix, vocab_file):
-        super(IntMemCompiler, self).__init__(input_files, output_prefix)
-        self.read_vocab(vocab_file)
-
-    def read_vocab(self, vocab_file):
-        with openall(vocab_file) as inf:
-            self.vocab = {w: i for i, w in enumerate(l.strip().split('\t')[0]
-                          for l in inf)}
-
-    def __call__(self):
-        """Does the work."""
-        with openall(self.output_prefix, 'wt') as header:
-            print('INT_MEM\t{}'.format(self.input_size), file=header)
-
-        arr = np.zeros((self.input_size), dtype=np.int32)
-        index = 0
-        for tokens in self.read_input(self.input_files):
-            for token in tokens:
-                arr[index] = self.vocab[token]
-                index += 1
-
-        np.savez(self.output_prefix, data=arr)
-
-
-class BatchIntMemCompiler(IntMemCompiler):
-    """
-    A kind of a mix between the two above. It is still an IntMemCompiler, but
-    the sentences are sorted by length and there is a batch parameter. Useful
-    for validation / test sets.
-    """
-    def __init__(self, input_files, output_prefix, vocab_file, batch_size):
-        super(BatchIntMemCompiler, self).__init__(
-            input_files, output_prefix, vocab_file)
-        self.batch_size = batch_size
-
-    def __call__(self):
-        """Does the work."""
-        with openall(self.output_prefix, 'wt') as header:
-            print('BATCH_INT_MEM\t{}\t{}'.format(self.batch_size, self.input_size),
-                  file=header)
-
-        data = [[self.vocab[token] for token in tokens]
-                for tokens in self.read_input(self.input_files)]
-        # data = sorted(data, key=lambda s: -len(s))
-
-        out_lists = [[] for _ in range(self.batch_size)]
+    def balance_batches(self, sentences):
+        """
+        Balances the batches: always puts a new sentence into the shortest
+        batch.
+        """
         lens = np.zeros((self.batch_size), dtype=int)
-        for s in data:
+        for sentence in sentences:
             i = lens.argmin()
-            out_lists[i].append(s)
-            lens[i] += len(s)
-        print(lens)
+            self.batches[i].append(sentence)
+            lens[i] += len(sentence)
 
-        # So that we don't keep the data 3 times in memory
-        del data
-        min_length = lens.min()
+    def input_size(self):
+        return sum(map(len, self.batches))
+
+
+class DefaultCorpusCompiler(CorpusCompiler):
+    """
+    Creates batches from the data.
+
+    Note that this algorithm is very rudimentary, and the chunks will be shorter
+    than what might be possible with the optimal (perhaps DP-based) one.
+    """
+    def compile_corpus(self):
+        sentences = self.read_input()
+        data_len = sum(map(len, sentences))
+        cut_len = data_len / float(self.batch_size)
+        i, lenb, curr_cut = 0, 0, cut_len
+        for sentence in sentences:
+            lens = len(sentence)
+            if lenb + lens <= curr_cut:
+                lenb += lens
+                self.batches[i].append(sentence)
+            elif i + 1 < len(self.batches):
+                i += 1
+                lenb += lens
+                curr_cut += cut_len
+                self.batches[i].append(sentence)
+            else:
+                break
+
+
+class ShuffledCorpusCompiler(CorpusCompiler):
+    """Creates shuffled batches from the data."""
+    def compile_corpus(self, seed=42):
+        sentences = self.read_input()
+        random.seed(seed)
+        random.shuffle(sentences)
+        self.balance_batches(sentences)
+        del sentences
+
+
+class SortedCorpusCompiler(CorpusCompiler):
+    """Creates sorted batches from the data."""
+    def compile_corpus(self):
+        # The sentences are reversed twice, because balance_batches works better
+        # it it can start with the longer sentences, but ultimately we want the
+        # shorter sentences at the beginning
+        sentences = self.read_input()
+        sentences.sort(key=len, reverse=True)
+        self.balance_batches(sentences)
+        del sentences
+        for batch in self.batches:
+            batch.reverse()
+
+
+class OutputWriter(object):
+    def __init__(self, output_prefix):
+        super(OutputWriter, self).__init__()
+        self.output_prefix = output_prefix
+        self.otype = get_name_stem(self.__class__, 'OutputWriter')
+
+    def write_output(self, *args):
+        raise NotImplementedError('write_output must be implemented')
+
+    def write_header(self):
+        with openall(self.output_prefix, 'wt') as header:
+            print(
+                '{}\t{}\t{}\t{}'.format(self.otype, self.ctype,
+                                        self.batch_size, self.input_size()),
+                file=header
+            )
+
+
+class IntOutputWriter(OutputWriter):
+    def __read_vocab(self, vocab_file):
+        with openall(vocab_file) as inf:
+            return {w: i for i, w in enumerate(l.strip().split('\t')[0]
+                    for l in inf)}
+
+    def write_output(self, vocab_file):
+        vocab = self.__read_vocab(vocab_file)
+        self.write_header()
+        min_length = min(map(len, self.batches))
         out_array = np.zeros((self.batch_size, min_length), dtype=np.int32)
-        for i in range(self.batch_size):
-            out_array[i] = np.fromiter(chain(*out_lists[i]), np.int32)[:min_length]
-
+        for i, batch in enumerate(self.batches):
+            out_array[i] = np.fromiter(
+                map(vocab.get, chain(*batch)), np.int32)[:min_length]
         np.savez(self.output_prefix, data=out_array)
 
 
-def parse_arguments():
+class TxtOutputWriter(OutputWriter):
+    def write_output(self, *args):
+        self.write_header()
+        out_ext = digits_format_str(self.batch_size)
+        for i in range(self.batch_size):
+            with openall(self.output_prefix + out_ext.format(i), 'wt') as outf:
+                for sentence in self.batches[i]:
+                    print(' '.join(sentence), file=outf)
+
+
+def parse_arguments(compilers, formats):
     parser = ArgumentParser(
         description='Prepares data for training.')
     parser.add_argument('input_files', nargs='+',
@@ -150,26 +160,26 @@ def parse_arguments():
                         help='the prefix of the output files\' names. It can '
                              'be a full path, in which case the directory '
                              'structure will be constructed, if needed.')
-    parser.add_argument('--format', '-f',
-                        choices=['txt_disk', 'int_mem', 'batch_int_mem'],
-                        help='the data format. The two choices are txt_disk, '
+    parser.add_argument('--compiler', '-c', choices=compilers.keys(),
+                        required=True,
+                        help='how the sentences are compiled.')
+    parser.add_argument('--format', '-f', choices=formats.keys(),
+                        required=True,
+                        help='the data format. The two choices are txt, '
                              'where the data is on disk in tokenized text '
-                             'format, and int_mem, where the data is in '
+                             'format, and int, where the data is in '
                              'memory as an array of ints.')
-    parser.add_argument('--batch-size', '-b', type=int, default=None,
+    parser.add_argument('--batch-size', '-b', type=int, required=True,
                         help='the training batch size. Only valid for the '
                              'txt_disk format.')
     parser.add_argument('--vocab-file', '-v', default=None,
                         help='the vocabulary file, created by count_vocab.py. '
-                             'Only needed for the int_mem format.')
+                             'Only needed for the int format.')
     args = parser.parse_args()
 
-    if args.format == 'txt_disk' and not args.batch_size:
-        parser.error('The number of batches is a required argument of the '
-                     'txt_disk format.')
-    if args.format == 'int_mem' and not args.vocab_file:
+    if args.format == 'int' and not args.vocab_file:
         parser.error('The vocabulary file is a required argument of the '
-                     'int_mem format.')
+                     'int format.')
 
     return args
 
@@ -184,17 +194,28 @@ def create_dir(output_prefix):
         os.makedirs(dir_prefix)
 
 
+def get_subclasses(cls):
+    clsmembers = inspect.getmembers(sys.modules[__name__], inspect.isclass)
+    return [c for _, c in clsmembers if issubclass(c, cls) and c != cls]
+
+
+def get_name_stem(cls, suffix):
+    return next(
+        filter(lambda c: suffix in c.__name__, inspect.getmro(cls))
+    ).__name__[:-len(suffix)].lower()
+
+
 def main():
-    args = parse_arguments()
+    compilers = {get_name_stem(c, 'CorpusCompiler'): c
+                 for c in get_subclasses(CorpusCompiler)}
+    formats = {get_name_stem(c, 'OutputWriter'): c
+               for c in get_subclasses(OutputWriter)}
+    args = parse_arguments(compilers, formats)
     create_dir(args.output_prefix)
-    if args.format == 'txt_disk':
-        c = TxtDiskCompiler(args.input_files, args.output_prefix, args.batch_size)
-    elif args.format == 'int_mem':
-        c = IntMemCompiler(args.input_files, args.output_prefix, args.vocab_file)
-    else:
-        c = BatchIntMemCompiler(args.input_files, args.output_prefix,
-                                args.vocab_file, args.batch_size)
-    c()
+    Worker = type('Worker', (compilers[args.compiler], formats[args.format]), {})
+    worker = Worker(args.input_files, args.batch_size, args.output_prefix)
+    worker.compile_corpus()
+    worker.write_output(args.vocab_file)
 
 
 if __name__ == '__main__':
